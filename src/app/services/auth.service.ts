a198dea701d59c8385e1826f70153ce99d385371
unknown <klencodes@gmail.com>
2021-03-17 09:15:37 +0000

    store current user details on localStore to avoid login on page reload

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -11,51 +11,54 @@ import { User } from '../models/user';
   providedIn: 'root',
 })
 export class AuthService {
-
+  
   private userSubject: BehaviorSubject<User>;
   public user: Observable<User>;
 
-  constructor(
-      private router: Router,
-      private http: HttpClient
-  ) {
-      this.userSubject = new BehaviorSubject<User>(null);
-      this.user = this.userSubject.asObservable();
+  constructor(private router: Router, private http: HttpClient) {
+
+    this.userSubject = new BehaviorSubject<any>(JSON.parse(localStorage.getItem('currentUser')));
+
+    // this.userSubject = new BehaviorSubject<User>(null);
+    this.user = this.userSubject.asObservable();
   }
 
   public get userValue(): User {
-      return this.userSubject.value;
+    return this.userSubject.value;
   }
 
   login(email: string, password: string) {
-      let url = environment.BASE_URL+environment.LOGIN;
-      return this.http.post<any>(url, { email, password }, { withCredentials: true }, )
-          .pipe(map(user => {
-              this.userSubject.next(user);
-              // this.startRefreshTokenTimer();
-              return user;
-          }));
+    let url = environment.BASE_URL + environment.LOGIN;
+    return this.http
+      .post<any>(url, { email, password }, { withCredentials: true })
+      .pipe(
+        map((user) => {
+          // store user details and jwt token in local storage to keep user logged in between page refreshes
+          localStorage.setItem('currentUser', JSON.stringify(user));
+          this.userSubject.next(user);
+          // this.startRefreshTokenTimer();
+          return user;
+        })
+      );
   }
 
   logout() {
-    let url = environment.BASE_URL+environment.LOGOUT;
+    let url = environment.BASE_URL + environment.LOGOUT;
     this.http.post<any>(url, {}, { withCredentials: true }).subscribe();
-      this.userSubject.next(null);
-      this.router.navigate(['/login']);
+    // remove user from local storage and set current user to null
+    localStorage.removeItem('currentUser');
+    this.userSubject.next(null);
+    this.router.navigate(['/login']);
   }
 
-
   signup(user: User) {
-    let url = environment.BASE_URL+environment.SIGNUP;
+    let url = environment.BASE_URL + environment.SIGNUP;
     return this.http.post(url, user);
   }
 
-  getAllFeeds(){
-    let url = environment.BASE_URL+environment.MAINFEEDS;
-    return this.http.get(url, { withCredentials: true },)
-    .pipe(map(res => {
-      return res;
-    })
-    )}
+  getAllFeeds() {
+    let url = environment.BASE_URL + environment.MAINFEEDS;
+    return this.http.get(url)
+  }
 
 }
